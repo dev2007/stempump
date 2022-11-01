6ab9fd325073c773f316da841216d526c0e3b825
awu <softengineerwu@gmail.com>
2022-11-01 08:35:38 +0800

    modify link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,9 +24,9 @@ module.exports = {
   themeConfig: {
     logo: '/favicon.png',
     nav: [
-      { text: '文档首页', link: '/' },
-      { text: 'BookHub 书库', link: 'https://pdf.bookhub.tech' },
-      { text: '中文文档汇集', link: 'https://docs.bookhub.tech' },
+      { text: 'BookHub 首页', link: 'https://www.bookhub.tech' },
+      { text: '中文文档', link: 'https://docs.bookhub.tech' },
+      { text: '计算机书库', link: 'https://pdf.bookhub.tech' },
       { text: 'GitHub', link: 'https://github.com/dev2007/stempump' }
     ],
     sidebar: [
